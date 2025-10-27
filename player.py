4627b6d49ac6fa1300050c70982674d87b73f68f
Dongil Park <rmaksssss@gmail.com>
2025-10-27 19:02:44 +0900

    칼 애니메이션 구현 완료

--- player.py
+++ player.py
@@ -3,12 +3,22 @@ from pico2d import *
 open_canvas()
 
 character = load_image('char1_1.png')
-
+sword = load_image('basic_sword.png')
+'''
 frame = 0
 while True:
     clear_canvas()
     character.clip_draw(frame * 30, 30, 30, 30, 400, 300, 50, 50)
     frame = (frame + 1) % 3
+    update_canvas()
+    delay(0.1)
+'''
+
+frame = 0
+while True:
+    clear_canvas()
+    sword.clip_draw(frame * 203, 0, 203, 350, 400, 300, 50, 50)
+    frame = (frame + 1) % 10
     update_canvas()
     delay(0.1)
 
