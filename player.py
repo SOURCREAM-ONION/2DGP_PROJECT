dd9a2c5733ec5df7369420ecc3ba1351ce05e561
Dongil Park <rmaksssss@gmail.com>
2025-10-27 18:28:41 +0900

    character 이미지가 나오게 구현 완료

--- player.py
+++ player.py
@@ -2,4 +2,11 @@ from pico2d import *
 
 open_canvas()
 
+character = load_image('char1_1.png')
+
+clear_canvas()
+character.clip_draw(0,0,30,30,400,300,200,200)
+update_canvas()
+delay(1)
+
 close_canvas()
