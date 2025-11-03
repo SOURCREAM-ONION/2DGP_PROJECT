4699970f3ab6cd30c1c514f73cc8bc0816872459
Dongil Park <rmaksssss@gmail.com>
2025-11-03 16:25:17 +0900

    Character 클래스 생성

--- player.py
+++ player.py
@@ -2,19 +2,11 @@ from pico2d import *
 
 open_canvas()
 
-character = load_image('char1_1.png')
-sword = load_image('basic_sword.png')
-
-framec = 0;
-frames = 0
-while True:
-    clear_canvas()
-    character.clip_draw(framec * 30, 30, 30, 30, 400, 300, 50, 50)
-    framec = (framec + 1) % 3
-    sword.clip_draw(frames * 203, 0, 203, 350, 410, 310, 50, 50)
-    frames = (frames + 1) % 10
-    update_canvas()
-    delay(0.3)
-
-
+class Character:
+    def __init__(self, image):
+        self.image = load_image('char1_1.png')
+    def draw(self):
+        pass
+    def update(self):
+        pass
 close_canvas()
