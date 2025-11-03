657359acf31f0cad8d0b3986f31ffcb6b7c2e441
Dongil Park <rmaksssss@gmail.com>
2025-11-03 16:28:47 +0900

    게임이 구현 될 수 있게 reset,update,render world 함수 생성

--- player.py
+++ player.py
@@ -6,7 +6,15 @@ class Character:
     def __init__(self, image):
         self.image = load_image('char1_1.png')
     def draw(self):
-        pass
+        self.image.draw(400, 30)
     def update(self):
         pass
+
+def reset_world():
+    pass
+def update_world():
+    pass
+def render_world():
+    pass
+
 close_canvas()
