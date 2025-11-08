7971a91a9057cef70997120322499d7c1551d7b2
Dongil Park <rmaksssss@gmail.com>
2025-11-08 16:50:20 +0900

    IDLE 상태에서 ATTACK상태로 가게끔 구현

--- character.py
+++ character.py
@@ -59,12 +59,17 @@ class Character:
         self.frame = 0 # 캐릭터의 프레임 초기화
         self.image = load_image('Char1_1.png') # 캐릭터의 이미지 로드
         self.IDLE = Idle(self) # Idle 상태 생성
-        self.state_machine = StateMachine(self.IDLE) #상태 머신 생성 및 초기 시작 상태 설정 (Idle로 초기 설정)
         self.ATTACK = Attack(self) # Attack 상태 생성
         self.DEFENCE = Defence(self) # Defence 상태 생성
+        self.state_machine = StateMachine(
+            self.IDLE,
+            {
+                self.IDLE : {self.ATTACK}
+            }
+        )
 
     def update(self):
-        self.state_machine.update() # 상태 머신한테 update를 맡김
+        self.state_machine.update()  # 상태 머신한테 update를 맡김
 
     def draw(self):  # 캐릭터가 그려지는 부분
         self.state_machine.draw() # 상태 머신한테 draw를 맡김
