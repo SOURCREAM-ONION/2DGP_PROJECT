5a8a04d42fd6f9c8bc18c0f4eb3b792a67bd2d8f
Dongil Park <rmaksssss@gmail.com>
2025-11-11 00:02:38 +0900

    Idle_Sword 상태에서 검을 들고 있는 모습을 구현

--- sword.py
+++ sword.py
@@ -4,10 +4,12 @@ from character import Character
 
 class Idle_Sword:
     def __init__(self, sword):
-        pass
+        self.sword = sword
+        self.frame = 0 # 검 대기 애니메이션 프레임 초기화
+        self.frame_count = 6 # 검 대기 애니메이션 프레임 수
 
     def enter(self):
-        pass
+        self.frame = 0
 
     def exit(self):
         pass
@@ -16,7 +18,8 @@ class Idle_Sword:
         pass
 
     def draw(self):
-        pass
+        import math
+        self.sword.image.clip_composite_draw(0, 0, 122, 122, -math.pi / 2, '' ,self.sword.x, self.sword.y, 50, 50)
 
 class Wield_Sword:
     def __init__(self, sword):
@@ -36,7 +39,7 @@ class Wield_Sword:
 
 class Sword:
     def __init__(self):
-        self.x, self.y = 400, 90 # 검의 초기 위치
+        self.x, self.y = 405, 87 # 검의 초기 위치
         self.image = load_image('basic_sword.png') # 검의 이미지 로드
         self.IDLE_SWORD = Idle_Sword(self)
         self.WIELD_SWORD = Wield_Sword(self)
