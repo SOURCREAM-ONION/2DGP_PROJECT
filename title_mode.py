eb25cccc4aae56337d78e2cec521ce9ed19591a9
Dongil Park <rmaksssss@gmail.com>
2025-11-11 23:44:38 +0900

    esc 누르면 타이틀 화면으로 넘어오게 수정

--- title_mode.py
+++ title_mode.py
@@ -1,5 +1,6 @@
 from pico2d import *
 import game_framework
+import play_mode
 
 image = None
 
@@ -18,6 +19,8 @@ def handle_events():
             game_framework.quit()
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
                 game_framework.quit()
+        elif (event.type, event.key) == (SDL_KEYDOWN, SDLK_SPACE):
+            game_framework.change_mode(play_mode)
 
 def draw():
     clear_canvas()
