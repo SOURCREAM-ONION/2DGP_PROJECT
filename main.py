080654bdf884d1646abd2dae3d9bd49e10eaca0d
Dongil Park <rmaksssss@gmail.com>
2025-11-11 23:39:21 +0900

    logo_mode에서 start_mode로 변경

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from pico2d import *
-import logo_mode
+import logo_mode as start_mode
 import game_framework
 
 open_canvas()
-game_framework.run(logo_mode)
+game_framework.run(start_mode)
 close_canvas()
