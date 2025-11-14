52327813fbbbf94afbf752469da52ac3d935f4dc
Dongil Park <rmaksssss@gmail.com>
2025-11-14 13:21:53 +0900

    building 클래스 생성

---
+++ building.py
@@ -0,0 +1,17 @@
+from pico2d import *
+
+class Building:
+    def __init__(self):
+        pass
+
+    def enter(self):
+        pass
+
+    def exit(self):
+        pass
+
+    def do(self):
+        pass
+
+    def draw(self):
+        pass
