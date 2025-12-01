dddb76e9c0e20bdff09a38bed0ec4fe554d587a7
Dongil Park <rmaksssss@gmail.com>
2025-12-01 22:59:37 +0900

    game_over시 esc를 누르면 타이틀 화면으로 이동하도록 구현

--- game_over.py
+++ game_over.py
@@ -17,7 +17,10 @@ def update():
     pass
 
 def handle_events():
-    pass
+    event_list = get_events()
+    for event in event_list:
+        if event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+            game_framework.change_mode(title_mode)
 
 def draw():
     pass
